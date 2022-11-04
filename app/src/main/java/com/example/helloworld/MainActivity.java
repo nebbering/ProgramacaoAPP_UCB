86dfe8ef45db13a2637f5fa8b47b622c0ccb3ec1
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-11-04 15:17:03 -0300

    up

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -24,55 +24,10 @@ import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    EditText txtA;
-    EditText txtB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
-        System.out.println(textView.getText());
-        txtA = (EditText) findViewById(R.id.txtA);
-        txtB = (EditText) findViewById(R.id.txtB);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-
-        float number1 = 6;
-        float number2 = 4;
-        Operacoes operacoes = new Operacoes(number1, number2);
-        System.out.println(operacoes.soma());
-        System.out.println(operacoes.subtracao());
-        System.out.println(operacoes.multiplicacao());
-        System.out.println(operacoes.divisao());
-        public void onSoma (View view){
-            int a = Integer.parseInt(txtA.getText().toString());
-            int b = Integer.parseInt(txtB.getText().toString());
-            Operacoes operacoes = new Operacoes(a, b);
-            textView.setText(String.valueOf(operacoes.soma()));
-        }
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            int id = item.getItemId();
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
     }
 }
